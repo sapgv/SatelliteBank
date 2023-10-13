5dae0152a5439a87bdaadd9a7c8f4318ec1ca550
Grigory Sapogov <grisha.sapgv@mail.ru>
2023-10-13 17:41:47 +0500

    Сделал ContentScrollView.

---
+++ SatelliteBank/UI/ContentScrollView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentScrollView.swift
+//  SatelliteBank
+//
+//  Created by Grigory Sapogov on 13.10.2023.
+//
+
+import UIKit
+
+final class ContentScrollView: UIScrollView {
+    
+    private(set) lazy var wrapperView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.layout()
+    }
+    
+    func layout() {
+        
+        self.wrapperView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(wrapperView)
+        
+        self.wrapperView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0).isActive = true
+        self.wrapperView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
+        self.wrapperView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
+        self.wrapperView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+        self.wrapperView.widthAnchor.constraint(equalTo: self.widthAnchor, constant: 0).isActive = true
+        
+        
+    }
+    
+}
--- SatelliteBank/ViewControllers/LoginViewController/LoginViewController.swift
+++ SatelliteBank/ViewControllers/LoginViewController/LoginViewController.swift
@@ -26,9 +26,11 @@ final class LoginViewController: UIViewController {
     
     private lazy var alternativeLogin: LoginViewAlternativeLoginButton = LoginViewAlternativeLoginButton()
     
-    private lazy var scrollView = UIScrollView()
+    private lazy var scrollView = ContentScrollView()
     
-    private lazy var wrapperView = UIView()
+    private var contentView: UIView {
+        scrollView.wrapperView
+    }
     
     private(set) var bottomConstraint: NSLayoutConstraint?
     
@@ -107,8 +109,26 @@ extension LoginViewController {
  
     private func layoutView() {
         
+        self.layoutScrollView()
+        self.layoutContent()
+        
+    }
+    
+    private func layoutScrollView() {
+        
         self.scrollView.translatesAutoresizingMaskIntoConstraints = false
-        self.wrapperView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(scrollView)
+        
+        self.scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+        self.scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        self.scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+        self.bottomConstraint = self.scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        self.bottomConstraint?.isActive = true
+        
+    }
+    
+    private func layoutContent() {
         
         self.skipButton.translatesAutoresizingMaskIntoConstraints = false
         self.logoImageView.translatesAutoresizingMaskIntoConstraints = false
@@ -118,63 +138,47 @@ extension LoginViewController {
         self.messageTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         self.alternativeLogin.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(scrollView)
-        self.scrollView.addSubview(wrapperView)
-
-        self.wrapperView.addSubview(self.skipButton)
-        self.wrapperView.addSubview(self.logoImageView)
-        self.wrapperView.addSubview(self.loginTextFieldView)
-        self.wrapperView.addSubview(self.submitButton)
-        self.wrapperView.addSubview(self.phoneTitleLabel)
-        self.wrapperView.addSubview(self.messageTitleLabel)
-        self.wrapperView.addSubview(self.alternativeLogin)
-
-        self.scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
-        self.scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
-        self.scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
-        self.bottomConstraint = self.scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
-        self.bottomConstraint?.isActive = true
-
-        self.wrapperView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor, constant: 0).isActive = true
-        self.wrapperView.topAnchor.constraint(equalTo: self.scrollView.topAnchor, constant: 0).isActive = true
-        self.wrapperView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor, constant: 0).isActive = true
-        self.wrapperView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor, constant: 0).isActive = true
-
-        self.wrapperView.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor, constant: 0).isActive = true
+        self.contentView.addSubview(self.skipButton)
+        self.contentView.addSubview(self.logoImageView)
+        self.contentView.addSubview(self.loginTextFieldView)
+        self.contentView.addSubview(self.submitButton)
+        self.contentView.addSubview(self.phoneTitleLabel)
+        self.contentView.addSubview(self.messageTitleLabel)
+        self.contentView.addSubview(self.alternativeLogin)
         
-        self.skipButton.topAnchor.constraint(equalTo: self.wrapperView.topAnchor, constant: padding).isActive = true
-        self.skipButton.trailingAnchor.constraint(equalTo: self.wrapperView.trailingAnchor, constant: -padding).isActive = true
+        self.skipButton.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: padding).isActive = true
+        self.skipButton.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -padding).isActive = true
         self.skipButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
 
         self.logoImageView.topAnchor.constraint(equalTo: self.skipButton.bottomAnchor, constant: padding).isActive = true
-        self.logoImageView.centerXAnchor.constraint(equalTo: self.wrapperView.centerXAnchor, constant: 0).isActive = true
+        self.logoImageView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor, constant: 0).isActive = true
         self.logoImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
 
-        self.phoneTitleLabel.leadingAnchor.constraint(equalTo: self.wrapperView.leadingAnchor, constant: padding).isActive = true
-        self.phoneTitleLabel.trailingAnchor.constraint(equalTo: self.wrapperView.trailingAnchor, constant: -padding).isActive = true
+        self.phoneTitleLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: padding).isActive = true
+        self.phoneTitleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -padding).isActive = true
         self.phoneTitleLabel.topAnchor.constraint(equalTo: self.logoImageView.bottomAnchor, constant: padding).isActive = true
         self.phoneTitleLabel.heightAnchor.constraint(equalToConstant: 44).isActive = true
 
-        self.messageTitleLabel.leadingAnchor.constraint(equalTo: self.wrapperView.leadingAnchor, constant: padding).isActive = true
-        self.messageTitleLabel.trailingAnchor.constraint(equalTo: self.wrapperView.trailingAnchor, constant: -padding).isActive = true
+        self.messageTitleLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: padding).isActive = true
+        self.messageTitleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -padding).isActive = true
         self.messageTitleLabel.topAnchor.constraint(equalTo: self.phoneTitleLabel.bottomAnchor, constant: padding).isActive = true
         self.messageTitleLabel.heightAnchor.constraint(equalToConstant: 44).isActive = true
 
-        self.loginTextFieldView.leadingAnchor.constraint(equalTo: self.wrapperView.leadingAnchor, constant: padding).isActive = true
-        self.loginTextFieldView.trailingAnchor.constraint(equalTo: self.wrapperView.trailingAnchor, constant: -padding).isActive = true
+        self.loginTextFieldView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: padding).isActive = true
+        self.loginTextFieldView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -padding).isActive = true
         self.loginTextFieldView.topAnchor.constraint(equalTo: self.messageTitleLabel.bottomAnchor, constant: padding).isActive = true
         self.loginTextFieldView.heightAnchor.constraint(equalToConstant: 64).isActive = true
 
-        self.submitButton.leadingAnchor.constraint(equalTo: self.wrapperView.leadingAnchor, constant: padding).isActive = true
+        self.submitButton.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: padding).isActive = true
         self.submitButton.topAnchor.constraint(equalTo: self.loginTextFieldView.bottomAnchor, constant: padding).isActive = true
-        self.submitButton.trailingAnchor.constraint(equalTo: self.wrapperView.trailingAnchor, constant: -padding).isActive = true
+        self.submitButton.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -padding).isActive = true
         self.submitButton.heightAnchor.constraint(equalTo: self.loginTextFieldView.heightAnchor).isActive = true
 
-        self.alternativeLogin.leadingAnchor.constraint(equalTo: self.wrapperView.leadingAnchor, constant: padding).isActive = true
+        self.alternativeLogin.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: padding).isActive = true
         self.alternativeLogin.topAnchor.constraint(equalTo: self.submitButton.bottomAnchor, constant: padding).isActive = true
-        self.alternativeLogin.trailingAnchor.constraint(equalTo: self.wrapperView.trailingAnchor, constant: -padding).isActive = true
+        self.alternativeLogin.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -padding).isActive = true
 
-        self.wrapperViewBottomConstraint = self.alternativeLogin.bottomAnchor.constraint(equalTo: self.wrapperView.bottomAnchor, constant: -padding)
+        self.wrapperViewBottomConstraint = self.alternativeLogin.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -padding)
         self.wrapperViewBottomConstraint?.isActive = true
         
     }
